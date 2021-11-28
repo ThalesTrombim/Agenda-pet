cd83b4cc1b26288715affb599de6e238cdaf6685
ThalesTrombim <thalestr@outlook.com>
2021-11-28 20:19:42 -0300

    api de cpf adicionada

--- infra/tables.js
+++ infra/tables.js
@@ -9,7 +9,7 @@ class tables {
     createSO(){
         const sql = `CREATE TABLE IF NOT EXISTS atendimentos (
             id int NOT NULL AUTO_INCREMENT,
-            cliente varchar(50) NOT NULL,
+            cliente varchar(11) NOT NULL,
             pet varchar(20),
             servico varchar(20) NOT NULL,
             data datetime NOT NULL, 
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,3 +1,4 @@
+const { default: axios } = require('axios');
 const moment = require('moment');
 
 const connection = require('../infra/connection');
@@ -56,11 +57,15 @@ class Atendimento {
     getById(id, res){
         const sql = `SELECT * FROM atendimentos WHERE id = ${id}`
 
-        connection.query(sql, (erro, result) => {
+        connection.query(sql, async (erro, result) => {
             const atendimento = result[0]
+            const cpf = atendimento.cliente;
+
             if(erro){
                 res.status(400).json(erro)
             } else {
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`);
+                atendimento.cliente = data
                 res.status(200).json(atendimento)
             }
         })
